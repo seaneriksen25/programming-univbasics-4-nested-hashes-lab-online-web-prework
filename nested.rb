7f153351d10dd250fb21b7020f7f92e8998eae3c
Sean Eriksen <eriksensean@yahoo.com>
2019-10-21 20:46:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay :known_for]
 end
 
 def dennis_ritchies_language
@@ -80,7 +82,10 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }]
 end
 
 def changing_alan
@@ -102,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay, :known_for] = alans_new_info = "GUI"
 end
 
 def adding_to_dennis
